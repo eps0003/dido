d1b90a82cd7b1e787cb848a7ff2583d4b58a6bd0
epsilon <eps0003@users.noreply.github.com>
2023-03-30 20:48:16 +1100

    Add CatchError module

---
+++ modules/catchError.ts
@@ -0,0 +1,21 @@
+import { Module } from "../middleware";
+
+/**
+ * Catches and handles thrown errors.
+ */
+export default class CatchError<Input, Output>
+  implements Module<Input, Output>
+{
+  constructor(
+    private module: Module<Input, Output>,
+    private errorModule: Module<{ data: Input; error: unknown }, Output>
+  ) {}
+
+  async process(data: Input): Promise<Output> {
+    try {
+      return await this.module.process(data);
+    } catch (error) {
+      return await this.errorModule.process({ data, error });
+    }
+  }
+}
