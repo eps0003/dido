d8ca463521f7ad01f0ea8813a41227f277916003
epsilon <eps0003@users.noreply.github.com>
2023-03-30 22:06:34 +1100

    Add RetryOnError module

---
+++ modules/retryOnError.ts
@@ -0,0 +1,30 @@
+import { Module } from "../middleware";
+import Literal from "./literal";
+
+/**
+ * Reprocesses the module if an error is thrown up to a specified maximum number of retries.
+ */
+export default class RetryOnError<Input, Output>
+  implements Module<Input, Output>
+{
+  constructor(
+    private module: Module<Input, Output>,
+    private maxRetries: Module<Input, number> = new Literal(3)
+  ) {}
+
+  async process(data: Input): Promise<Output> {
+    let retries = 0;
+    const maxRetries = await this.maxRetries.process(data);
+
+    while (true) {
+      try {
+        return await this.module.process(data);
+      } catch (error) {
+        if (retries >= maxRetries) {
+          throw error;
+        }
+        retries++;
+      }
+    }
+  }
+}
