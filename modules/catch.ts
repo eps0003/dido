caf2f4a13e82e25ec1c3a6dab9c2f029bf24b687
epsilon <eps0003@users.noreply.github.com>
2023-04-01 00:17:10 +1100

    Rename CatchError module to Catch

--- modules/catch.ts
+++ modules/catch.ts
@@ -3,19 +3,17 @@ import Module from "../module";
 /**
  * Catches and handles thrown errors.
  */
-export default class CatchError<Input, Output>
-  implements Module<Input, Output>
-{
+export default class Catch<Input, Output> implements Module<Input, Output> {
   constructor(
     private module: Module<Input, Output>,
-    private errorModule: Module<{ data: Input; error: unknown }, Output>
+    private handleError: Module<{ data: Input; error: unknown }, Output>
   ) {}
 
   async process(data: Input): Promise<Output> {
     try {
       return await this.module.process(data);
     } catch (error) {
-      return await this.errorModule.process({ data, error });
+      return await this.handleError.process({ data, error });
     }
   }
 }
