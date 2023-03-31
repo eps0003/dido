936a3ce2b7aa7f82ae803d61407a89491790bb69
epsilon <eps0003@users.noreply.github.com>
2023-04-01 00:00:53 +1100

    Convert private identifiers to private keyword

--- modules/fork.ts
+++ modules/fork.ts
@@ -4,14 +4,14 @@ import Module from "../module";
  * Runs modules at the same time, passing the same input to each module and returning the input once all modules have finished processing.
  */
 export default class Fork<T> implements Module<T, T> {
-  #modules: Module<T, T>[] = [];
+  private modules: Module<T, T>[] = [];
 
   constructor(...modules: Module<T, T>[]) {
-    this.#modules = modules;
+    this.modules = modules;
   }
 
   async process(data: T): Promise<T> {
-    const promises = this.#modules.map((module) => module.process(data));
+    const promises = this.modules.map((module) => module.process(data));
     await Promise.all(promises);
     return data;
   }
--- modules/pipe.ts
+++ modules/pipe.ts
@@ -4,22 +4,22 @@ import Module from "../module";
  * Runs modules in succession, passing the output of each module to the next as input.
  */
 export default class Pipe<Input, Output> implements Module<Input, Output> {
-  #modules: Module<any, any>[] = [];
+  private modules: Module<any, any>[] = [];
 
   constructor(module: Module<Input, Output>) {
-    this.#modules.push(module);
+    this.modules.push(module);
   }
 
   next<NextOutput>(
     module: Module<Output, NextOutput>
   ): Pipe<Input, NextOutput> {
-    this.#modules.push(module);
+    this.modules.push(module);
     return this as unknown as Pipe<Input, NextOutput>;
   }
 
   async process(data: Input): Promise<Output> {
     let d: unknown = data;
-    for (const module of this.#modules) {
+    for (const module of this.modules) {
       d = await module.process(d);
     }
     return d as Output;
