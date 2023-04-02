c850f42b4f687c3c10d1645c9fca21ec29194b56
epsilon <eps0003@users.noreply.github.com>
2023-04-02 11:03:21 +1000

    Use tuple in Catch module

--- modules/catch.ts
+++ modules/catch.ts
@@ -6,14 +6,14 @@ import Module from "../module";
 export default class Catch<Input, Output> implements Module<Input, Output> {
   constructor(
     private module: Module<Input, Output>,
-    private handleError: Module<{ data: Input; error: unknown }, Output>
+    private handleError: Module<[Input, unknown], Output>
   ) {}
 
   async process(data: Input): Promise<Output> {
     try {
       return await this.module.process(data);
     } catch (error) {
-      return await this.handleError.process({ data, error });
+      return await this.handleError.process([data, error]);
     }
   }
 }
