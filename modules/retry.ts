bcaeb5091003d1174744d7fe566a626ff3ab9830
epsilon <eps0003@users.noreply.github.com>
2023-04-03 22:52:23 +1000

    Add #onRetry() method to Retry module

--- modules/retry.ts
+++ modules/retry.ts
@@ -1,13 +1,21 @@
 import Module from "../module";
+import Fork from "./fork";
 
 /**
  * Reprocesses the module if an error is thrown up to a specified maximum number of retries.
  */
 export default class Retry<Input, Output> implements Module<Input, Output> {
+  private retryModules: Module<[Input, unknown], unknown>[] = [];
+
   constructor(
     private maxRetries: Module<Input, number>,
     private module: Module<Input, Output>
   ) {}
+
+  onRetry(module: Module<[Input, unknown], unknown>): this {
+    this.retryModules.push(module);
+    return this;
+  }
 
   async process(data: Input): Promise<Output> {
     let retries = 0;
@@ -21,6 +29,8 @@ export default class Retry<Input, Output> implements Module<Input, Output> {
           throw error;
         }
         retries++;
+
+        await new Fork(...this.retryModules).process([data, error]);
       }
     }
   }
