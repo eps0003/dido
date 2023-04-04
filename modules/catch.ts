b11a49700c90cb0e0a18180183a178bea0cb7016
epsilon <eps0003@users.noreply.github.com>
2023-04-04 23:03:45 +1000

    Use objects instead of tuples in Time and Catch modules

--- modules/catch.ts
+++ modules/catch.ts
@@ -1,19 +1,21 @@
 import Module from "../module";
 
+export type CatchOutput<T> = { data: T; error: unknown };
+
 /**
  * Catches and handles thrown errors.
  */
 export default class Catch<Input, Output> implements Module<Input, Output> {
   constructor(
     private module: Module<Input, Output>,
-    private handleError: Module<[Input, unknown], Output>
+    private handleError: Module<CatchOutput<Input>, Output>
   ) {}
 
   async process(data: Input): Promise<Output> {
     try {
       return await this.module.process(data);
     } catch (error) {
-      return await this.handleError.process([data, error]);
+      return await this.handleError.process({ data, error });
     }
   }
 }
--- modules/time.ts
+++ modules/time.ts
@@ -1,18 +1,20 @@
 import Module from "../module";
 
+export type TimeOutput<T> = { data: T; duration: number };
+
 /**
  * Processes the module and returns result along with how long it took to process in milliseconds.
  */
 export default class Time<Input, Output>
-  implements Module<Input, [Output, number]>
+  implements Module<Input, TimeOutput<Output>>
 {
   constructor(private module: Module<Input, Output>) {}
 
-  async process(data: Input): Promise<[Output, number]> {
+  async process(data: Input): Promise<TimeOutput<Output>> {
     const startTime = new Date().getTime();
     const result = await this.module.process(data);
     const endTime = new Date().getTime();
     const duration = endTime - startTime;
-    return [result, duration];
+    return { data: result, duration };
   }
 }
