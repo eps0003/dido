8cc2281f5feb7e7d50e77c7ef8999025d278bce9
epsilon <eps0003@users.noreply.github.com>
2023-04-10 16:37:57 +1000

    Add module utility types

--- module.ts
+++ module.ts
@@ -1,3 +1,17 @@
 export type Module<Input, Output> = {
   process(data: Input): Output | Promise<Output>;
 };
+
+export type InputType<T extends Module<unknown, unknown>> = T extends Module<
+  infer Input,
+  unknown
+>
+  ? Input
+  : never;
+
+export type OutputType<T extends Module<unknown, unknown>> = T extends Module<
+  unknown,
+  infer Output
+>
+  ? Output
+  : never;
